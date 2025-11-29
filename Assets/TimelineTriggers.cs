664eeaccd7be9cb1341982d25de737dacf5cee6c
gorejiraa <gorejiraa@gmail.com>
2025-11-28 23:34:05 -0400

    Fixed it

--- Assets/TimelineTriggers.cs
+++ Assets/TimelineTriggers.cs
@@ -4,58 +4,14 @@ using UnityEngine.Playables;
 public class TimelineTrigger : MonoBehaviour
 {
     public PlayableDirector timelineToPlay;
-    public bool triggerOnce = true;
-    public MonoBehaviour playerMovementScript; // Your movement script
-    public MonoBehaviour playerCameraScript;   // Your camera look script
-
-    private bool hasTriggered = false;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        Debug.Log("Triggered entered");
+        if (other.tag == "Player")
         {
-            if (triggerOnce && hasTriggered)
-                return;
-
-            if (timelineToPlay != null)
-            {
-                // Disable player movement
-                if (playerMovementScript != null)
-                {
-                    playerMovementScript.enabled = false;
-                }
-
-                // Disable camera control
-                if (playerCameraScript != null)
-                {
-                    playerCameraScript.enabled = false;
-                }
-
-                // Play timeline
-                timelineToPlay.Play();
-                hasTriggered = true;
-
-                // Subscribe to timeline finished event
-                timelineToPlay.stopped += OnTimelineFinished;
-            }
+            Debug.Log("Player in trigger");
+            timelineToPlay.Play();
         }
-    }
-
-    void OnTimelineFinished(PlayableDirector director)
-    {
-        // Re-enable player movement
-        if (playerMovementScript != null)
-        {
-            playerMovementScript.enabled = true;
-        }
-
-        // Re-enable camera control
-        if (playerCameraScript != null)
-        {
-            playerCameraScript.enabled = true;
-        }
-
-        // Unsubscribe from event
-        director.stopped -= OnTimelineFinished;
     }
 }
